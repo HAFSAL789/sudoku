d17fae0308b02a8ca0eb26445f6f288fc22bdf8c
HAFSAL <75556327+HAFSAL789@users.noreply.github.com>
2021-09-18 09:42:10 +0530

    sudoku 1.1-timer stopping added-.exe file size reduced into half

--- sudokugui.py
+++ sudokugui.py
@@ -4,63 +4,66 @@ import time
 import sudokualgo
 import os
 
-#general things
+# general things
 pygame.init()
 gamescreen = pygame.display.set_mode((500, 530))
 pygame.display.set_caption("sudoku")
-icon_img = pygame.image.load(os.path.join("fonts","icon.png"))
+icon_img = pygame.image.load(os.path.join("fonts", "icon.png"))
 pygame.display.set_icon(icon_img)
 
 # secondary board for highlighting red color around the possible places where user can put numbers
-board1 = [[5,3,0,0,7,0,0,0,0],
-         [6,0,0,1,9,5,0,0,0],
-         [0,9,8,0,0,0,0,6,0],
-         [8,0,0,0,6,0,0,0,3],
-         [4,0,0,8,0,3,0,0,1],
-         [7,0,0,0,2,0,0,0,6],
-         [0,6,0,0,0,0,2,8,0],
-         [0,0,0,4,1,9,0,0,5],
-         [0,0,0,0,8,0,0,7,9],
-         ]
+board1 = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+          [6, 0, 0, 1, 9, 5, 0, 0, 0],
+          [0, 9, 8, 0, 0, 0, 0, 6, 0],
+          [8, 0, 0, 0, 6, 0, 0, 0, 3],
+          [4, 0, 0, 8, 0, 3, 0, 0, 1],
+          [7, 0, 0, 0, 2, 0, 0, 0, 6],
+          [0, 6, 0, 0, 0, 0, 2, 8, 0],
+          [0, 0, 0, 4, 1, 9, 0, 0, 5],
+          [0, 0, 0, 0, 8, 0, 0, 7, 9],
+          ]
 
 
 # time function
 
 def timefun(seconds):
     sec = seconds % 60
-    min = seconds//60
-    hour = min//60
-    return str(int(hour))+":"+str(int(min))+":"+str(int(sec))
+    min = seconds // 60
+    hour = min // 60
+    return str(int(hour)) + ":" + str(int(min)) + ":" + str(int(sec))
 
 
-#to display won
+# to display won
 def win():
-    win_font = pygame.font.Font(os.path.join("fonts","starjedi.ttf"), 19)
+    win_font = pygame.font.Font(os.path.join("fonts", "starjedi.ttf"), 19)
     renderd_Win = win_font.render("won", True, (255, 191, 0))
-    return gamescreen.blit(renderd_Win,(350,0)),gamescreen.blit(renderd_Win,(150,0))
+    return gamescreen.blit(renderd_Win, (350, 0)), gamescreen.blit(renderd_Win, (150, 0))
 
-#to display wrong
+
+# to display wrong
 def failed():
-    win_font = pygame.font.Font(os.path.join("fonts","starjedi.ttf"), 19)
-    renderd_Win = win_font.render("Wrong", True, (255,0, 0))
+    win_font = pygame.font.Font(os.path.join("fonts", "starjedi.ttf"), 19)
+    renderd_Win = win_font.render("Wrong", True, (255, 0, 0))
     return gamescreen.blit(renderd_Win, (300, 0)), gamescreen.blit(renderd_Win, (150, 0))
 
-#create a button
-def buttonfun(color,size,border):
-    return pygame.draw.rect(gamescreen,color,size,border)
+
+# create a button
+def buttonfun(color, size, border):
+    return pygame.draw.rect(gamescreen, color, size, border)
+
 
 # second screen
 def sudokufun():
     start = time.time()
     gamescreen.fill((178, 190, 181))
-    #fonts
-    mainmenu = pygame.font.Font(os.path.join("fonts","MyUglyHandwriting-Regular.otf"),25)
+    # fonts
+    mainmenu = pygame.font.Font(os.path.join("fonts", "MyUglyHandwriting-Regular.otf"), 25)
     board_numbers = pygame.font.Font(os.path.join("fonts", "zector.ttf"), 30)
 
-
     # to keep looping until false
-    running=True
-
+    running = True
+    solve_false = True
+    stopped_time = 0
     while running:
         # mouse positions
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -104,21 +107,22 @@ def sudokufun():
                         board1[k][i] == 0:
 
                     # if key is pressed, it will pass to the board
-                    if key!=None:
+                    if key != None:
                         sudokualgo.board[k][i] = key
 
                     # to highlight the grid boders with red
-                    buttonfun((255,0,0), (i * 55.5, k * 55.5 + 30, 55.5, 55.5), 1)
+                    buttonfun((255, 0, 0), (i * 55.5, k * 55.5 + 30, 55.5, 55.5), 1)
 
                 else:
 
-                    #to highlight the gird with white
-                    buttonfun((255,255,255), (i * 55.5, k * 55.5 + 30, 55.5, 55.5), 0)
+                    # to highlight the gird with white
+                    buttonfun((255, 255, 255), (i * 55.5, k * 55.5 + 30, 55.5, 55.5), 0)
 
                     # to highlight grid borders
-                    if mouse_x in range(int(i*55.5),int(i*55.5+55.5)) and mouse_y in range(int(k * 55.5+30),int(k*55.5+30+55.5)):
+                    if mouse_x in range(int(i * 55.5), int(i * 55.5 + 55.5)) and mouse_y in range(int(k * 55.5 + 30),
+                                                                                                  int(k * 55.5 + 30 + 55.5)):
                         # with blue colour
-                        buttonfun((0,0,255), (i * 55.5, k * 55.5 + 30, 55.5, 55.5), 1)
+                        buttonfun((0, 0, 255), (i * 55.5, k * 55.5 + 30, 55.5, 55.5), 1)
 
                     else:
                         # with some weird colour, i think its ash idk.
@@ -127,14 +131,13 @@ def sudokufun():
                 # to add numbers from board to screen
                 if board1[k][i] == 0:
                     # in brown
-                    digit = board_numbers.render(str(sudokualgo.board[k][i]), True, (111,67,33))
+                    digit = board_numbers.render(str(sudokualgo.board[k][i]), True, (111, 67, 33))
 
                 else:
-                    #in black
+                    # in black
                     digit = board_numbers.render(str(sudokualgo.board[k][i]), True, (0, 0, 0))
 
                 gamescreen.blit(digit, (i * 55.5 + 20, k * 55.5 + 30 + 20))
-
 
         # to draw black solid lines
         pygame.draw.line(gamescreen, (0, 0, 0), (166, 0), (166, 600), 1)
@@ -146,23 +149,30 @@ def sudokufun():
         buttonfun((255, 255, 255), (0, 0, 600, 30), 0)
 
         # manage clock
-        timer = timefun(time.time()-start)
-        timerfont = pygame.font.Font(os.path.join("fonts","MyUglyHandwriting-Regular.otf"), 25).render(timer,True,(0,0,0))
-        gamescreen.blit(timerfont,(450,4))
+        if solve_false:
+            timer = timefun(time.time() - start)
+        else:
+            timer = stopped_time
+        timerfont = pygame.font.Font(os.path.join("fonts", "MyUglyHandwriting-Regular.otf"), 25).render(timer, True,
+                                                                                                        (0, 0, 0))
+        gamescreen.blit(timerfont, (450, 4))
 
         # solve button
         if mouse_x in range(250, 290) and mouse_y in range(5, 22):
             rendered_solve = mainmenu.render("solve", True, (178, 132, 190))
             # actually this should be inside a event loop but to save looping time i tried this way, it works somehow
-            if events.type == pygame.MOUSEBUTTONDOWN:
-                if events.button == 1:
-                    sudokualgo.board = copy.deepcopy(board1)
-                    sudokualgo.start()
-                    sudokualgo.board = copy.deepcopy(sudokualgo.copyboard)
+            if events.type == pygame.MOUSEBUTTONDOWN and events.button == 1:
+                sudokualgo.board = copy.deepcopy(board1)
+                sudokualgo.start()
+                sudokualgo.board = copy.deepcopy(sudokualgo.copyboard)
+                stopped_time = timer
+                solve_false = False
+
+
         else:
             rendered_solve = mainmenu.render("solve", True, (0, 0, 0))
 
-        #mainmenu button
+        # mainmenu button
         if mouse_x in range(0, 72) and mouse_y in range(4, 23):
             rendered_start = mainmenu.render('mainmenu', True, (178, 132, 190))
             if events.type == pygame.MOUSEBUTTONDOWN:
@@ -173,9 +183,9 @@ def sudokufun():
             rendered_start = mainmenu.render('mainmenu', True, (0, 0, 0))
 
         # to display both start and solve button
-        gamescreen.blit(rendered_start,(0,0))
+        gamescreen.blit(rendered_start, (0, 0))
         gamescreen.blit(rendered_solve, (250, 0))
-        
+
         # check whether user failed or not
         for solved in range(9):
             if 0 in sudokualgo.board[solved]:
@@ -185,54 +195,52 @@ def sudokufun():
                 failed()
             else:
                 win()
+            stopped_time = timer
+            solve_false = False
 
-        # to thank pygame
+        # Update the display
         pygame.display.update()
 
 
 # main function
 def mainfun():
-
-    #general things
-    gamescreen.fill((255,109,58))
-    font = pygame.font.Font(os.path.join("fonts","space age.ttf"),40)
-    font_button = pygame.font.Font(os.path.join("fonts","starjedi.ttf"),35)
-    text = font.render("sudoku",True,(255,255,255))
+    # general things
+    gamescreen.fill((255, 109, 58))
+    font = pygame.font.Font(os.path.join("fonts", "space age.ttf"), 40)
+    font_button = pygame.font.Font(os.path.join("fonts", "starjedi.ttf"), 35)
+    text = font.render("sudoku", True, (255, 255, 255))
 
     # newgame button
     buttonfun((255, 255, 255), (150, 217, 200, 30), 0)
     text_button = font_button.render("newgame", True, (255, 0, 0))
 
-
     run = True
     while run:
         gamescreen.blit(text, (140, 150))
         gamescreen.blit(text_button, (160, 200))
-        # again to thank pygame
+        # Update the display
         pygame.display.update()
 
-        mouse_pos_X,mouse_pos_y = pygame.mouse.get_pos()
+        mouse_pos_X, mouse_pos_y = pygame.mouse.get_pos()
         # to quit
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 run = False
 
             # clicking newgame will start sudokufun
-            if mouse_pos_X in range(150,350) and mouse_pos_y in range(217,247):
+            if mouse_pos_X in range(150, 350) and mouse_pos_y in range(217, 247):
                 if events.type == pygame.MOUSEBUTTONDOWN:
                     if events.button == 1:
                         sudokufun()
                         run = False
                 # button color red with white font
-                buttonfun((255,0,0),(150,217,200,30),0)
-                text_button = font_button.render("newgame", True, (255,255,255))
+                buttonfun((255, 0, 0), (150, 217, 200, 30), 0)
+                text_button = font_button.render("newgame", True, (255, 255, 255))
             else:
                 # button color white with red font
                 buttonfun((255, 255, 255), (150, 217, 200, 30), 0)
                 text_button = font_button.render("newgame", True, (255, 0, 0))
 
 
-
-mainfun()
-
-# its my first python and pygame project,if u saw any mistakes pls inform
+if __name__ == '__main__':
+    mainfun()
